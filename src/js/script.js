892270c405d65c0bba2ac267a31706884029030c
vasyl.pashko <vasyl.pashko@gmail.com>
2023-07-14 10:41:30 +0300

    styling timeline added animation

--- src/js/script.js
+++ src/js/script.js
@@ -27,6 +27,8 @@ function showSection(sectionId) {
   });
 }
 
+// Horizontal Scroll Pproject Cards
+
 const scrollingWrapper = document.querySelector(".scrolling-wrapper");
 
 scrollingWrapper.addEventListener("wheel", (event) => {
@@ -41,3 +43,35 @@ projectCard.forEach((card) => {
     this.classList.toggle("flipped");
   });
 });
+
+// TimeLine Animation
+
+const timelineItems = document.querySelectorAll(".timeline-item");
+
+function isInViewport(element) {
+  const skillsPageGrid = document.querySelector("#skills-page-grid");
+  if (!skillsPageGrid) {
+    return false;
+  }
+
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function checkTimelineItems() {
+  timelineItems.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("fade-in");
+    }
+  });
+}
+
+window.addEventListener("load", checkTimelineItems);
+
+window.addEventListener("scroll", checkTimelineItems);
